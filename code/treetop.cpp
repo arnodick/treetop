45539da924136f2504f044039eb0a193da5bdb72
Ashley Pringle <ash.pringle@gmail.com>
2015-02-18 23:25:42 -0400

    Almost done the window

--- code/treetop.cpp
+++ code/treetop.cpp
@@ -7,11 +7,68 @@
 
 #include <windows.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running; // global for now
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void 
+Win32ResizeDIBSection(int Width, int Height)
+{
+	// TODO: Bulletproof this
+	// Maybe don't free first, free after, then free first if that fails
+
+	if(BitmapHandle)
+	{
+		DeleteObject(BitmapHandle);
+	}
+	
+	if(!BitmapDeviceContext)
+	{
+		// TODO should we recreate these under certain special circumstances?
+		BitmapDeviceContext = CreateCompatibleDC(0);
+	}
+
+
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = Width;
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	BitmapHandle = CreateDIBSection(
+		BitmapDeviceContext,
+		&BitmapInfo,
+		DIB_RGB_COLORS,
+		&BitmapMemory,
+		0,
+		0);
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+	StretchDIBits(DeviceContext,
+						X, Y, Width, Height,
+						X, Y, Width, Height,
+						BitmapMemory,
+						&BitmapInfo,
+						DIB_RGB_COLORS, // change this to DIB_PAL_COLORS for palette based colour
+						SRCCOPY);
+}
+
 LRESULT CALLBACK
-MainWindowCallback(	HWND Window,
-					UINT Message,
-					WPARAM WParam,
-					LPARAM LParam)
+Win32MainWindowCallback(HWND Window,
+						UINT Message,
+						WPARAM WParam,
+						LPARAM LParam)
 {
 	LRESULT Result = 0;
 
@@ -19,22 +76,31 @@ MainWindowCallback(	HWND Window,
 	{
 		case WM_SIZE:
 		{
-			OutputDebugStringA("WM_SIZE\n");
-		} break;
-
-		case WM_DESTROY:
-		{
-			OutputDebugStringA("WM_DESTROY\n");
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+			int Height = ClientRect.bottom - ClientRect.top;
+			int Width = ClientRect.right - ClientRect.left;
+			Win32ResizeDIBSection(Width, Height);
+			//OutputDebugStringA("WM_SIZE\n");
 		} break;
 
 		case WM_CLOSE:
 		{
-			OutputDebugStringA("WM_CLOSE\n");
+			// handle this with message to the user
+			Running = false;
+			//OutputDebugStringA("WM_CLOSE\n");
 		} break;
 
 		case WM_ACTIVATEAPP:
 		{
 			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+
+		case WM_DESTROY:
+		{
+			// handle this as an error, and recreate window
+			Running = false;
+			//OutputDebugStringA("WM_DESTROY\n");
 		} break;
 
 		case WM_PAINT:
@@ -45,7 +111,9 @@ MainWindowCallback(	HWND Window,
 			int Y = Paint.rcPaint.top;
 			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
 			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-			static DWORD Operation = WHITENESS;
+			Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+			/*
+			local_persist DWORD Operation = WHITENESS;
 			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
 			if (Operation == WHITENESS)
 			{
@@ -55,8 +123,9 @@ MainWindowCallback(	HWND Window,
 			{
 				Operation = WHITENESS;
 			}
+			*/
 			EndPaint(Window, &Paint);
-		}break;
+		} break;
 		//SetPixel(DeviceContext, 100, 100, RGB(255, 0, 255)); // in theory puts a pixel on the screen at x == 100 y == 100, in the window DeviceContext(?) but can't see it on my screen
 		default:
 		{
@@ -78,7 +147,7 @@ WinMain(HINSTANCE Instance,
 	WNDCLASS WindowClass = {}; // {} is null, right? nothing input
 
 	//WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW; //do REDRAWS still matter? // removing this line glitches window when resizing it
-	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.lpfnWndProc = Win32MainWindowCallback;
 	WindowClass.hInstance = Instance;
 	WindowClass.lpszClassName = "TreetopWindowClass";
 	//WindowClass.hIcon = 0;
@@ -86,8 +155,8 @@ WinMain(HINSTANCE Instance,
 	if(RegisterClass(&WindowClass))
 	{
 		HWND WindowHandle = 
-			CreateWindowEx(
-				WS_EX_PALETTEWINDOW, // change this to 0 for normal behaviour
+			CreateWindowExA( // why ExA?
+				0, // change this to 0 for normal behaviour
 		  		WindowClass.lpszClassName,
 				"Treetop Fighter",
 				WS_OVERLAPPEDWINDOW|WS_VISIBLE,
@@ -101,12 +170,13 @@ WinMain(HINSTANCE Instance,
 				0);
 		if(WindowHandle)
 		{
-			for(;;) // infinite loop. without this, window will open, run to end of code, and finish.
+			Running = true;
+			while(Running) // infinite loop. without this, window will open, run to end of code, and finish.
 					// the for(;;) method is standard now
 					// the while(1) method is another way, but is not accepted standard any more. higher warning levels will give warnings about constant in while.
 			{
 				MSG Message;
-				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
 				if (MessageResult > 0)
 				{
 					TranslateMessage(&Message);
@@ -121,7 +191,7 @@ WinMain(HINSTANCE Instance,
 		else
 		{
 			// TODO
-		}		
+		}
 	}
 	else
 	{
